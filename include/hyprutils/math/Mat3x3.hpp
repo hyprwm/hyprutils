a9f85a4bca7fceabaa1f36a07a2b3416590986d7
Vaxry <43317083+vaxerski@users.noreply.github.com>
2024-09-24 18:18:25 +0100

    Math: add Mat3x3 (#14)

---
+++ include/hyprutils/math/Mat3x3.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <array>
+#include <vector>
+#include <string>
+#include <ostream>
+
+#include "./Misc.hpp"
+
+namespace Hyprutils {
+    namespace Math {
+        class CBox;
+        class Vector2D;
+
+        class Mat3x3 {
+          public:
+            Mat3x3();
+            Mat3x3(std::array<float, 9>);
+            Mat3x3(std::vector<float>);
+
+            /* create an identity 3x3 matrix */
+            static Mat3x3 identity();
+
+            /* create an output projection matrix */
+            static Mat3x3 outputProjection(const Vector2D& size, eTransform transform);
+
+            /* get the matrix as an array, in a RTL TTB order. */
+            std::array<float, 9> getMatrix() const;
+
+            /* create a box projection matrix */
+            Mat3x3 projectBox(const CBox& box, eTransform transform, float rot = 0.F /* rad, CCW */) const;
+
+            /* in-place functions */
+            Mat3x3& transform(eTransform transform);
+            Mat3x3& rotate(float rot /* rad, CCW */);
+            Mat3x3& scale(const Vector2D& scale);
+            Mat3x3& scale(const float scale);
+            Mat3x3& translate(const Vector2D& offset);
+            Mat3x3& transpose();
+            Mat3x3& multiply(const Mat3x3& other);
+
+            /* misc utils */
+            Mat3x3      copy() const;
+            std::string toString() const;
+
+            bool        operator==(const Mat3x3& other) const {
+                return other.matrix == matrix;
+            }
+
+            friend std::ostream& operator<<(std::ostream& os, const Mat3x3& mat) {
+                os << mat.toString();
+                return os;
+            }
+
+          private:
+            std::array<float, 9> matrix;
+        };
+    }
+}
--- tests/math.cpp
+++ tests/math.cpp
@@ -1,4 +1,5 @@
 #include <hyprutils/math/Region.hpp>
+#include <hyprutils/math/Mat3x3.hpp>
 #include "shared.hpp"
 
 using namespace Hyprutils::Math;
@@ -83,6 +84,15 @@ int main(int argc, char** argv, char** envp) {
         EXPECT(box.containsPoint(Vector2D(60, 60)), false);
         EXPECT(box.overlaps(CBox(25, 25, 50, 50)), true);
         EXPECT(box.inside(CBox(0, 0, 100, 100)), false);
+    }
+
+    // Test matrices
+    {
+        Mat3x3 jeremy = Mat3x3::outputProjection({1920, 1080}, HYPRUTILS_TRANSFORM_FLIPPED_90);
+        Mat3x3 matrixBox = jeremy.projectBox(CBox{10, 10, 200, 200}, HYPRUTILS_TRANSFORM_NORMAL).translate({100, 100}).scale({1.25F, 1.5F}).transpose();
+
+        Mat3x3 expected = std::array<float, 9>{0, 0.46296296, 0, 0.3125, 0, 0, 19.84375, 36.055557, 1};
+        EXPECT(matrixBox, expected);
     }
 
     return ret;
