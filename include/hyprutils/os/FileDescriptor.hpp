0538b9455d47ac0c976c0f3c34be0d0fcb2832b5
Tom Englund <tomenglund26@gmail.com>
2024-11-20 13:50:10 +0100

    filedescriptor: implent a new FileDescriptor class
    makes you able to RAII the filedescriptor making it somewhat easier to
    not leak.

---
+++ include/hyprutils/os/FileDescriptor.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <fcntl.h>
+namespace Hyprutils {
+    namespace OS {
+        class CFileDescriptor {
+          public:
+            CFileDescriptor() = default;
+            explicit CFileDescriptor(int const fd);
+            CFileDescriptor(CFileDescriptor&&);
+            CFileDescriptor& operator=(CFileDescriptor&&);
+            ~CFileDescriptor();
+
+            CFileDescriptor(const CFileDescriptor&)            = delete;
+            CFileDescriptor& operator=(const CFileDescriptor&) = delete;
+
+            bool             operator==(const CFileDescriptor& rhs) const {
+                return m_fd == rhs.m_fd;
+            }
+
+            bool            isValid() const;
+            int             get() const;
+            int             getFlags() const;
+            bool            setFlags(int flags);
+            int             take();
+            void            reset();
+            CFileDescriptor duplicate(int flags = F_DUPFD_CLOEXEC) const;
+
+            bool            isReadable() const;
+            bool            isClosed() const;
+
+            static bool     isReadable(int fd);
+            static bool     isClosed(int fd);
+
+          private:
+            int m_fd = -1;
+        };
+    };
+};
---
+++ src/os/FileDescriptor.cpp
@@ -0,0 +1,89 @@
+#include <cstdlib>
+#include <hyprutils/os/FileDescriptor.hpp>
+#include <fcntl.h>
+#include <sys/poll.h>
+#include <unistd.h>
+#include <utility>
+
+using namespace Hyprutils::OS;
+
+CFileDescriptor::CFileDescriptor(int const fd) : m_fd(fd) {}
+
+CFileDescriptor::CFileDescriptor(CFileDescriptor&& other) : m_fd(std::exchange(other.m_fd, -1)) {}
+
+CFileDescriptor& CFileDescriptor::operator=(CFileDescriptor&& other) {
+    if (this == &other) // Shit will go haywire if there is duplicate ownership
+        abort();
+
+    reset();
+    m_fd = std::exchange(other.m_fd, -1);
+    return *this;
+}
+
+CFileDescriptor::~CFileDescriptor() {
+    reset();
+}
+
+bool CFileDescriptor::isValid() const {
+    return m_fd != -1;
+}
+
+int CFileDescriptor::get() const {
+    return m_fd;
+}
+
+int CFileDescriptor::getFlags() const {
+    return fcntl(m_fd, F_GETFD);
+}
+
+bool CFileDescriptor::setFlags(int flags) {
+    if (fcntl(m_fd, F_SETFD, flags) == -1)
+        return false;
+
+    return true;
+}
+
+int CFileDescriptor::take() {
+    return std::exchange(m_fd, -1);
+}
+
+void CFileDescriptor::reset() {
+    if (m_fd != -1) {
+        close(m_fd);
+        m_fd = -1;
+    }
+}
+
+CFileDescriptor CFileDescriptor::duplicate(int flags) const {
+    if (m_fd == -1)
+        return {};
+
+    return CFileDescriptor{fcntl(m_fd, flags, 0)};
+}
+
+bool CFileDescriptor::isClosed() const {
+    return isClosed(m_fd);
+}
+
+bool CFileDescriptor::isReadable() const {
+    return isReadable(m_fd);
+}
+
+bool CFileDescriptor::isClosed(int fd) {
+    pollfd pfd = {
+        .fd      = fd,
+        .events  = POLLIN,
+        .revents = 0,
+    };
+
+    if (poll(&pfd, 1, 0) < 0)
+        return true;
+
+    return pfd.revents & (POLLHUP | POLLERR);
+}
+
+bool CFileDescriptor::isReadable(int fd) {
+    pollfd pfd = {.fd = fd, .events = POLLIN, .revents = 0};
+
+    return poll(&pfd, 1, 0) > 0 && (pfd.revents & POLLIN);
+}
