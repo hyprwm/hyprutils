59b320523e1e5afedb0b0149f5ae0c3f3bb5bc8e
littleblack111 <littleblack11111@gmail.com>
2025-01-09 18:42:32 +0800

    fix

--- include/hyprutils/animation/AnimationManager.hpp
+++ include/hyprutils/animation/AnimationManager.hpp
@@ -14,6 +14,7 @@ namespace Hyprutils {
         class CAnimationManager {
           public:
             CAnimationManager();
+			virtual ~CAnimationManager() = default;
 
             void                                                                         tickDone();
             bool                                                                         shouldTickForNext();
