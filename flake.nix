e63f3a79334dec49f8eb1691f66f18115df04085
Mihai Fufezan <mihai@fufexan.net>
2026-03-02 15:57:50 +0200

    treewide: alejandra -> nixfmt

--- flake.nix
+++ flake.nix
@@ -6,26 +6,31 @@
     systems.url = "github:nix-systems/default-linux";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    systems,
-  }: let
-    inherit (nixpkgs) lib;
-    eachSystem = lib.genAttrs (import systems);
-    pkgsFor = eachSystem (system:
-      import nixpkgs {
-        localSystem.system = system;
-        overlays = with self.overlays; [hyprutils];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = lib.genAttrs (import systems);
+      pkgsFor = eachSystem (
+        system:
+        import nixpkgs {
+          localSystem.system = system;
+          overlays = with self.overlays; [ hyprutils ];
+        }
+      );
+    in
+    {
+      overlays = import ./nix/overlays.nix { inherit self lib; };
+
+      packages = eachSystem (system: {
+        default = self.packages.${system}.hyprutils;
+        inherit (pkgsFor.${system}) hyprutils hyprutils-debug hyprutils-with-tests;
       });
-  in {
-    overlays = import ./nix/overlays.nix {inherit self lib;};
 
-    packages = eachSystem (system: {
-      default = self.packages.${system}.hyprutils;
-      inherit (pkgsFor.${system}) hyprutils hyprutils-debug hyprutils-with-tests;
-    });
-
-    formatter = eachSystem (system: pkgsFor.${system}.alejandra);
-  };
+      formatter = eachSystem (system: pkgsFor.${system}.nixfmt-tree);
+    };
 }
